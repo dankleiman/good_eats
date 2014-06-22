7256e6fb5f3b767b7fd9e7e67f30c0545b3689b7
Dan Kleiman <info@dankleiman.com>
2014-06-21 20:23:54 -0400

    New restaurant form, with links to and from.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :reviews, only: :index
-  resources :restaurants, only: :index
+  resources :restaurants, only: [:index, :new, :create]
 
   root 'reviews#index'
 end
