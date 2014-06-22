99489503f9f0e4a25c2a216ef51400a310235507
Dan Kleiman <info@dankleiman.com>
2014-06-21 20:34:42 -0400

    Added new restaurant form and validations to save restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,4 +6,21 @@ class RestaurantsController < ApplicationController
   def new
     @restaurant = Restaurant.new
   end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      flash[:notice] = "Successfully added restaurant."
+      redirect_to restaurants_path
+    else
+      flash[:notice] = "Could not add restaurant."
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :city, :state, :zip, :description, :category)
+  end
 end
