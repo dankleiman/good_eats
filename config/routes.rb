37649fd18009708b855859354631f7a1ca30c894
Dan Kleiman <info@dankleiman.com>
2014-06-21 20:47:38 -0400

    Restaurant show pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :reviews, only: :index
-  resources :restaurants, only: [:index, :new, :create]
+  resources :restaurants, only: [:index, :new, :create, :show]
 
   root 'reviews#index'
 end
